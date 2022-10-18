8e810929ba5da7b84f9614fd8fc2e89f712ce4c9
OxyRRR <ksenijarepina230488@gmail.com>
2022-10-18 19:59:52 +0500

    !!!!

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var sideA: Int = 5
+    var sideB: Double = 7.5
+    var sideC: Int = 10
+    var sizeV: Double = sideA * sideB * sideC
+
+    println("Обём параллелепипеда составляет " + sizeV + " кубических метров")
+
+}
